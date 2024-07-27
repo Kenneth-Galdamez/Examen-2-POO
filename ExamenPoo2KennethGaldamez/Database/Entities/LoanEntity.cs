94d254813e5d81583ee29bfdffe337bfe2937b91
Kenneth-Galdamez <kengalguevara@gmail.com>
2024-07-27 07:14:31 -0600

    Actualizar LoanEntity.cs

--- ExamenPoo2KennethGaldamez/Database/Entities/LoanEntity.cs
+++ ExamenPoo2KennethGaldamez/Database/Entities/LoanEntity.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExamenPoo2KennethGaldamez.Database.Entities
@@ -12,16 +12,22 @@ namespace ExamenPoo2KennethGaldamez.Database.Entities
         public float Loan_amount { get; set; }
 
         [Required]
-        [Column("comission_rate")]
-        public float tasa_comision { get; set; }
-
-        [Required]
         [Column("interest_rate")]
         public float tasa_interes { get; set; }
 
         [Required]
-        [Column("plazos")]
-        public int plazo { get; set; }
+        [Column("plazo_meses")]
+        public int plazo_meses { get; set; }
+
+        [Required]
+        [Column("firstpaymentdate")]
+        public DateTime firstpaymentdate { get; set; }
+
+
+        [Required]
+        [Column("disbursementdate")]
+        public DateTime disbursementdate { get; set; }
+
 
         [Column("client_id")]
         public Guid ClientId { get; set; }
